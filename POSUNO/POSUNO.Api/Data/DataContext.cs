4c22c9be58f681cbc5e8c73c2fa50924068145ec
Rui Ferreira <ruif.sps@gmail.com>
2021-05-02 00:21:57 +0100

    T65 - Parte 02

---
+++ POSUNO/POSUNO.Api/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POSUNO.Api.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasIndex(e => e.Name).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(e => e.Email).IsUnique();
+        }
+    }
+}
